8ae4db34a22a7a7fa974c6b21664013538a60ade
vijay324 <vk606532@gmail.com>
2023-11-08 18:27:13 +0530

    first commit

---
+++ email-marketing/xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://demo.smartwpthemes.com/email-marketing</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://demo.smartwpthemes.com/email-marketing/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://demo.smartwpthemes.com/email-marketing/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://demo.smartwpthemes.com/email-marketing/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://demo.smartwpthemes.com/email-marketing/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://demo.smartwpthemes.com/email-marketing/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
